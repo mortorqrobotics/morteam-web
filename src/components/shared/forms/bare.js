c2cae4d234fad0a7fbed7f94ef11b53c9806184f
thatarvinguy <zadeharvin@gmail.com>
2016-07-10 15:39:12 -0700

    Create file structure for react

---
+++ src/components/shared/forms/bare.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+let self;
+
+//Import components
+import SubComponentName from "./path";
+
+//Import styles
+import styles from "./path";
+
+export default class ComponentName extends React.Component {
+
+	constructor(props) {
+		super(props);
+        self = this;
+		self.state = {
+
+		};
+	}
+
+    //Handle Actions
+
+    //Render
+	render() {
+
+	}
+}
---
+++ src/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+window.React = React;
+
+ReactDOM.render(<div>hello</div>, document.getElementById("test"));
+
+async function test() {
+    await stuff();
+    return 4;
+}
+
+test();
