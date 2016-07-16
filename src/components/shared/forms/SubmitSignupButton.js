7af4486201a3e01c2c220117b8e84a53d684e5f2
thatarvinguy <zadeharvin@gmail.com>
2016-07-16 00:42:22 -0700

    Add SubmitSignupButton component

---
+++ src/components/shared/forms/SubmitSignupButton.js
@@ -0,0 +1,29 @@
+
+var styles = {
+    submitButton: {
+      "backgroundColor": "#0099FF",
+      "border": "0",
+      "borderRadius": "1px",
+      "fontSize": "32px",
+      "position": "absolute",
+      "width": "300px",
+      "left": "50%",
+      "marginLeft": "-150px",
+      "height": "50px",
+      "top": "530px",
+      "boxShadow": "1.5px 3px 8px -2px #BA7000"
+    }
+}
+
+@Radium
+export default class SubmitSignupButton extends React.Component {
+
+    render() {
+        return (
+            <SubmitButton
+                value="Submit"
+                style={styles.submitButton}
+            />
+        )
+    }
+}
