8de7c920461a2abc32174c37d2dde3032a7f215a
thatarvinguy <zadeharvin@gmail.com>
2016-07-15 23:31:31 -0700

    Add many missing styles

--- src/components/signup/SignupInput.js
+++ src/components/signup/SignupInput.js
@@ -10,6 +10,18 @@ var styles = {
     "signup_item_not_make_team_question": {
       "boxShadow": "1.5px 3px 8px -2px #BA7000",
       "borderRadius": "0px"
+  },
+    "signup_item_extended": {
+        "position": "relative",
+        "left": "50%",
+        "width": "300px",
+        "height": "40px",
+        "font-size": "25px",
+        "margin-left": "-150px",
+        "margin-top": "20px",
+        "border": "0",
+        "border-radius": "5px",
+        "padding": "8px"
     }
 }
 
@@ -27,7 +39,7 @@ export default class SignupInput extends React.Component {
             <TextBox
                 placeholder={this.props.placeholder}
                 onChange={this.props.onChange}
-                style={[styles.signup_item, styles.signup_item_not_make_team_question]}
+                style={[styles.signup_item, styles.signup_item_not_make_team_question, styles.signup_item_extended]}
             />
         )
     }
