630fd643247650893611af352cb412ae47639653
thatarvinguy <zadeharvin@gmail.com>
2016-07-16 00:35:38 -0700

    Add SubmitButton component

--- src/components/shared/forms/SubmitButton.js
+++ src/components/shared/forms/SubmitButton.js
@@ -1,1 +1,21 @@
 
+@Radium
+export default class SubmitButton extends React.Component {
+
+    static propTypes = {
+        value: React.PropTypes.string,
+        onChange: React.PropTypes.func,
+        style: React.PropTypes.object
+    }
+
+    render() {
+        return (
+            <input
+                type="submit"
+                value={this.props.text}
+                onChange={this.props.onChange}
+                style={this.props.style}
+            />
+        )
+    }
+}
