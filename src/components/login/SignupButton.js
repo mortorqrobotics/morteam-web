5ca792952c90d697ea4a4024b899ee6ce7b7342d
thatarvinguy <zadeharvin@gmail.com>
2016-07-22 13:10:33 -0700

    Add unfinished styles to signup button

--- src/components/login/SignupButton.js
+++ src/components/login/SignupButton.js
@@ -2,6 +2,22 @@ import React from "react";
 
 import Button from "~/components/shared/forms/Button";
 
+let styles = {
+    signupButton: {
+        backgroundColor: "#0099FF",
+    	border: "0",
+    	borderRadius: "1px",
+    	fontSize: "32px",
+    	marginTop: "20px",
+    	position: "relative",
+    	width: "300px",
+    	left: "50%",
+    	marginLeft: "-150px",
+    	height: "50px",
+    	boxShadow: "1.5px 3px 8px -2px #BA7000",
+    }
+}
+
 export default class LoginButton extends React.Component {
 
     onClick() {
@@ -10,7 +26,7 @@ export default class LoginButton extends React.Component {
 
     render() {
         return (
-            <Button text="Sign Up" onClick={this.onClick} />
+            <Button style={[styles.signupButton]}text="Sign Up" onClick={this.onClick} />
         )
     }
 }
