2c9188087e5fb876c6ef0a11865141b6f859da74
thatarvinguy <zadeharvin@gmail.com>
2016-07-22 13:14:51 -0700

    Add hover to signup button

--- src/components/login/SignupButton.js
+++ src/components/login/SignupButton.js
@@ -4,7 +4,7 @@ import Button from "~/components/shared/forms/Button";
 
 let styles = {
     signupButton: {
-        backgroundColor: "#0099FF",
+        backgroundColor: "#00A2FF",
     	border: "0",
     	borderRadius: "1px",
     	fontSize: "32px",
@@ -15,6 +15,9 @@ let styles = {
     	marginLeft: "-150px",
     	height: "50px",
     	boxShadow: "1.5px 3px 8px -2px #BA7000",
+        ":hover":{
+            backgroundColor: "#008BDB",
+        }
     }
 }
 
