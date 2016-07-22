b9509995b499774ed44e5063073b95d04dfb919e
afuous <benheller42@gmail.com>
2016-07-22 11:15:18 -0700

    Add LoginButton and make login work

--- src/components/login/LoginBox.js
+++ src/components/login/LoginBox.js
@@ -6,7 +6,9 @@ import { makeChangeHandlerFactory } from "~/util";
 import LoginUsernameBox from "~/components/login/LoginUsernameBox";
 import LoginPasswordBox from "~/components/login/LoginPasswordBox";
 import LoginRememberMeBox from "~/components/login/LoginRememberMeBox";
+import LoginButton from "~/components/login/LoginButton";
 import Link from "~/components/shared/Link";
+import Form from "~/components/shared/forms/Form";
 
 let styles = {
     loginBox: {
@@ -46,7 +48,7 @@ export default class LoginBox extends React.Component {
         try {
             let { data } = await ajax.request("post", ajax.getRoute("login"), {
                 username: this.state.username,
-                password: this.state.passsord,
+                password: this.state.password,
                 rememberMe: this.state.checkedRM,
             });
             console.log(data);
@@ -59,23 +61,28 @@ export default class LoginBox extends React.Component {
     render() {
         return (
             <div style={styles.loginBox}>
+                
+                <Form onSubmit={this.onSubmit}>
 
-                <LoginUsernameBox
-                    value={this.state.username}
-                    onChange={this.getChangeHandler("username")}
-                />
-                <br/>
-                <LoginPasswordBox
-                    value={this.state.password}
-                    onChange={this.getChangeHandler("password")}
-                />
-                <br/>
-                <LoginRememberMeBox
-                    checked={this.state.checkedRM}
-                    onChange={this.getChangeHandler("checkedRM", "checked")}
-                />
-                <br/>
+                    <LoginUsernameBox
+                        value={this.state.username}
+                        onChange={this.getChangeHandler("username")}
+                    />
+                    <br/>
+                    <LoginPasswordBox
+                        value={this.state.password}
+                        onChange={this.getChangeHandler("password")}
+                    />
+                    <br/>
+                    <LoginRememberMeBox
+                        checked={this.state.checkedRM}
+                        onChange={this.getChangeHandler("checkedRM", "checked")}
+                    />
+                    <br/>
 
+                    <LoginButton />
+
+                </Form>
 
     			<Link style={styles.fpLink} location="/fp" text="Forgot password?" />
     		</div>
---
+++ src/components/login/LoginButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import SubmitButton from "~/components/shared/forms/SubmitButton";
+
+export default class LoginButton extends React.Component {
+
+    render() {
+        return (
+            <SubmitButton value="Login" />
+        )
+    }
+}
