f0d07015755b677b682579fb2e006dcbd2ee05e9
afuous <benheller42@gmail.com>
2016-07-28 22:01:58 -0700

    Change niceditor usage

--- src/components/index/editor/Editor.js
+++ src/components/index/editor/Editor.js
@@ -16,22 +16,41 @@ let styles = {
 @Radium
 export default class Editor extends React.Component {
 
-    constructor(props) {
-        super(props);
+    componentDidMount() {
+        bkLib.onDomLoaded(function() {
+            new nicEditor({
+                buttonList: [
+                    "bold",
+                    "italic",
+                    "underline",
+                    "left",
+                    "center",
+                    "right",
+                    "justify",
+                    "removeformat",
+                    "hr",
+                    "upload",
+                ]
+            }).panelInstance("niceditor-textarea");
+        });
+    }
 
+    shouldComponentUpdate() {
+        return false;
+        // bad stuff will happen if this component rerenders
+        // the js in componentDidMount will get undone
+        // and the user input will be wiped out
+        // should probably find a better way to do this...
     }
 
     render() {
         return (
             <div>
-                <textarea style={styles.textarea} id="main-textarea" placeholder="Make an announcement..."></textarea>
-                <script type="text/javascript">
-                    {bkLib.onDomLoaded(function() {
-                        new nicEditor({
-                            buttonList: ['bold', 'italic', 'underline', 'left', 'center', 'right', 'justify', 'removeformat', 'hr', 'upload']
-                        }).panelInstance('main-textarea');
-                    })}
-                </script>
+                <textarea
+                    style={styles.textarea}
+                    id="niceditor-textarea"
+                    placeholder="Make an announcement..."
+                />
             </div>
         )
     }
