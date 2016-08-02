5343e72ca62770702eb8b1e5c17725f61d1e9963
thatarvinguy <zadeharvin@gmail.com>
2016-08-01 23:53:17 -0700

    Import Navbar where needed

--- src/components/index/Index.js
+++ src/components/index/Index.js
@@ -5,6 +5,7 @@ import Root, { pageInit } from "~/components/shared/Root";
 import Leftbar from "./leftbar/Leftbar";
 import AnnouncementsList from "./announcements/AnnouncementsList";
 import Editor from "./editor/Editor";
+import Navbar from "~/components/shared/navbar/Navbar"
 
 let styles = {
     div: {
