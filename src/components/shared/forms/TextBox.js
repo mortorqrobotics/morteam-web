118103fcc6e60eda522977f4ed42cd965b097432
thatarvinguy <zadeharvin@gmail.com>
2016-08-19 16:55:14 -0700

    Add more options for TextBox type

--- src/components/shared/forms/TextBox.js
+++ src/components/shared/forms/TextBox.js
@@ -2,9 +2,13 @@ import React from "react";
 import Radium from "radium";
 
 const TextBox = (props) => {
-    let { autoCapitalize, autoCorrect, ...rest } = props;
+    let { autoCapitalize, autoCorrect, type, ...rest } = props;
+    type = type || "text";
+    if (["text", "number", "email", "password"].indexOf(type) == -1){
+        throw new Error("Invalid type prop for TextBox");
+    }
     let newProps = {
-        type: "text",
+        type: type,
         ...rest,
     };
     if (typeof autoCapitalize === "boolean") {
