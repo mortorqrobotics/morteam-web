eda9a9b00ac7c9408fcbd0d0142472eb126e19c6
tiffany1618 <tiffany9342@gmail.com>
2016-08-24 03:12:46 -0700

    Add more group stuff

--- src/group/components/Group.js
+++ src/group/components/Group.js
@@ -7,40 +7,57 @@ import GroupHeading from "./GroupHeading";
 import GroupMember from "./GroupMember";
 import LeaveGroupButton from "./LeaveGroupButton";
 import InviteMemberButton from "./InviteMemberButton";
-import styles from "~/group/styles";
+import styles from "./styles/index";
 import Navbar from "~/shared/components/navbar/Navbar";
 
 @Radium
 export default class Group extends React.Component {
-    
-    constructor(props) {
-        super(props);
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: null,
+      group: null,
     }
-    
+  }
+
     componentDidMount = async() => {
         try {
-            let [userRes, userGroupRes, publicGroupRes] = await Promise.all([
-                ajax.request("get", ""),
-                ajax.request("get", "")
-            ]);
+            let [ { data: users, }, { data: group, } ] = await Promise.all([
+              ajax.request("get", "/teams/current/users");
+              ajax.request("get", "/groups");
+            ])
+            this.setState({
+              users: users,
+              group: group,
+            })
         } catch (err) {
             console.log(err);
         }
     }
-    
+
     render() {
         return(
             <Root pageName="group">
                 <Navbar />
-                <GroupHeading />
+                <GroupHeading
+                  group={group}
+                />
                 <LeaveGroupButton />
                 <InviteMemberButton />
                 <div>
+                  {this.state.users.map(user => (
+                    <GroupMember
+                      user:{user}
+                      key={user._id}
+                    />
+                  ))}
                 </div>
             </Root>
         )
     }
-    
+
 }
 
 pageInit(Group);
