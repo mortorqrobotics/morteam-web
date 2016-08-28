8dcfe281746c6cacf16666f19352a9f7f87854fc
afuous <benheller42@gmail.com>
2016-08-27 22:12:05 -0700

    Fix bug with extra empty month in calendar

--- src/util/date.js
+++ src/util/date.js
@@ -32,7 +32,7 @@ export const nextAbsMonth = ({ month, year }) => {
 export const prevAbsMonth = ({ month, year }) => {
     if (month == 0) {
         return {
-            month: 12,
+            month: 11,
             year: year - 1,
         }
     } else {
