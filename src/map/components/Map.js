94e0bd250f95a3050f142616563e53598dfcc79a
afuous <benheller42@gmail.com>
2016-09-10 07:26:50 -0700

    Add config file and locations

--- src/map/components/Map.js
+++ src/map/components/Map.js
@@ -5,11 +5,13 @@ import GoogleMap from "google-map-react";
 import Root, { pageInit } from "~/shared/components/Root";
 import Navbar from "~/shared/components/navbar/Navbar";
 import Leftbar from "~/map/components/Leftbar";
+import config from "~/../config.json";
 
 @Radium
 class Map extends React.Component {
 
     render() {
+                console.log("A")
         return (
             <Root pageName="map">
                 <Navbar />
@@ -17,7 +19,21 @@ class Map extends React.Component {
                 <GoogleMap
                     center={[39.9583086, -98.3331244]}
                     zoom={4}
-                />
+                    bootstrapURLKeys={{
+                        key: config.mapsKey,
+                    }}
+                >
+                    {Object.keys(teamLocations).map(teamNum => (
+                        // yes they are switched
+                        <div
+                            key={teamNum}
+                            lat={teamLocations[teamNum].longitude}
+                            lng={teamLocations[teamNum].latitude}
+                        >
+                            {teamNum}
+                        </div>
+                    ))}
+                </GoogleMap>
             </Root>
         )
     }
