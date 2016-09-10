ea881deccc943bf285fc4af18209af9540158eea
AriellaS <ariella.simoni@gmail.com>
2016-09-09 19:29:40 -0700

    Add google map to map page

--- src/map/components/Map.js
+++ src/map/components/Map.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Radium from "radium";
 
+import GoogleMap from "google-map-react";
 import Root, { pageInit } from "~/shared/components/Root";
 import Navbar from "~/shared/components/navbar/Navbar";
 import Leftbar from "~/map/components/Leftbar";
@@ -13,6 +14,10 @@ class Map extends React.Component {
             <Root pageName="map">
                 <Navbar />
                 <Leftbar />
+                <GoogleMap
+                    center={[39.9583086, -98.3331244]}
+                    zoom={4}
+                />
             </Root>
         )
     }
