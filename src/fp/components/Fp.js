8d8e5d3a9cb8c3b426d2b3bad0dbeed45ca498ca
E-A-A <ev34an@gmail.com>
2016-09-25 09:31:04 +0000

    Add functionality to fp

--- src/fp/components/Fp.js
+++ src/fp/components/Fp.js
@@ -4,10 +4,12 @@ import ajax from "~/util/ajax";
 import Link from "~/shared/components/Link";
 import TextBox from "~/shared/components/forms/TextBox";
 import SubmitButton from "~/shared/components/forms/SubmitButton";
-import Form from "~/shared/components/forms/Button";
+import Form from "~/shared/components/forms/Form";
+import ErrorMsg from "~/shared/components/forms/ErrorMsg";
 
 import Root, { pageInit } from "~/shared/components/Root";
 import { makeChangeHandlerFactory, REDIR_TIME } from "~/util";
+import ajax from "~/util/ajax";
 
 import styles from "~/fp/styles";
 
@@ -19,13 +21,32 @@ class Fp extends React.Component {
     state = {
         email: "",
         username: "",
+        errorMsg: "",
     }
     
+    onSubmit = async() =>{
+        try{    
+            let {data} = await ajax.request("post", "/forgotPassword", {
+                email: this.state.email, 
+                username: this.state.username,
+            });
+            this.setState({
+                errorMsg: "sucess",
+            });
+            setTimeout(() => window.location.assign("/"), REDIR_TIME);
+            
+        } catch ({ response: { data } }) {
+            this.setState({
+                errorMsg: data
+            });
+        }
+        
+    }
     render() {
         return (
             <Root pageName="fp">
                 <Link location="login" text="Back to login" />
-                <Form style={styles.form}>  
+                <Form style={styles.form} onSubmit={this.onSubmit}>  
                     <TextBox 
                         placeholder="Email"
                         style={styles.textBox}
@@ -43,6 +64,7 @@ class Fp extends React.Component {
                         styles={styles.button}
                         text="OK"
                     />
+                    <ErrorMsg message={this.state.errorMsg} />
                 </Form>
             </Root>
         )
