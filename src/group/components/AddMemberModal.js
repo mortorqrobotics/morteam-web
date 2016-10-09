5b0dc4ea10334bef0a12ca1648727e8f914c4a52
AriellaS <ariella.simoni@gmail.com>
2016-10-09 03:48:16 -0700

    Make improvements to add group member modal

---
+++ src/group/components/AddMemberModal.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Radium from "radium";
+
+import styles from "~/group/styles/index";
+import ajax from "~/util/ajax";
+import { ModalButton } from "~/shared/components/modal";
+import StandardModal from "~/shared/components/StandardModal";
+import AudienceSelect from "~/shared/components/audience/AudienceSelect";
+import { modalPropTypes, modalPropsForward } from "~/util/modal";
+import { connect } from "react-redux";
+import { addUsers } from "~/group/actions";
+
+@Radium
+class AddMemberModal extends React.Component {
+
+    static propTypes = {
+        ...modalPropTypes,
+    }
+
+    initialState = {
+        users: [],
+    }
+
+    state = {
+        allUsers: [],
+        ...this.initialState,
+    }
+
+    componentDidMount = async () => {
+        const { data } = await ajax.request("get", "/teams/current/users");
+        this.setState({ allUsers: data, });
+    }
+
+    onSubmit = () => {
+        this.props.dispatch(addUsers(this.state.users));
+        this.setState(this.initialState);
+        this.props.onRequestClose();
+    }
+
+    render() {
+        return (
+            <div>
+                <StandardModal
+                    title="Add Members"
+                    { ...modalPropsForward(this) }
+                >
+
+                    <AudienceSelect
+                        userList={this.state.allUsers.filter(u => (
+                            !this.props.users.some(gu => (
+                                gu._id == u._id
+                            ))
+                        ))}
+                        selected={{ groups: [], users: this.state.users, }}
+                        onChange={({ users }) => this.setState({ users })}
+                    />
+
+                    <ModalButton
+                        text="Add"
+                        onClick={this.onSubmit}
+                    />
+
+                </StandardModal>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        users: state.users
+    }
+}
+
+export default connect(mapStateToProps)(AddMemberModal);
--- src/group/components/InviteMemberButton.js
+++ src/group/components/InviteMemberButton.js
@@ -3,30 +3,15 @@ import Radium from "radium";
 
 import styles from "~/group/styles/index";
 import Button from "~/shared/components/forms/Button";
-import ajax from "~/util/ajax";
-import { ModalButton } from "~/shared/components/modal";
-import StandardModal from "~/shared/components/StandardModal";
-import AudienceSelect from "~/shared/components/audience/AudienceSelect";
-import { pageOptions } from "~/util";
+import AddMemberModal from "~/group/components/AddMemberModal";
+import { modalProps } from "~/util/modal";
 import { connect } from "react-redux";
-import { addUsers } from "~/group/actions";
 
 @Radium
 class InviteMemberButton extends React.Component {
 
     state = {
         isModalOpen: false,
-        users: [],
-        allUsers: [],
-    }
-
-    componentDidMount = async () => {
-        const { data } = await ajax.request("get", "/teams/current/users");
-        this.setState({ allUsers: data, });
-    }
-
-    addUsers = () => {
-        this.props.dispatch(addUsers(this.state.users));
     }
 
     render() {
@@ -37,38 +22,13 @@ class InviteMemberButton extends React.Component {
                     value="Add Members"
                     onClick={() => this.setState({ isModalOpen: true, })}
                 />
-                <StandardModal
-                    title="Add Members"
-                    isOpen={this.state.isModalOpen}
-                    onAfterOpen={() => this.setState({ isModalOpen: true, })}
-                    onRequestClose={() => this.setState({ isModalOpen: false, })}
-                >
 
-                    <AudienceSelect
-                        userList={this.state.allUsers.filter(u => (
-                            !this.props.users.some(gu => (
-                                gu._id == u._id
-                            ))
-                        ))}
-                        selected={{ groups: [], users: this.state.users, }}
-                        onChange={({ users }) => this.setState({ users })}
-                    />
-
-                    <ModalButton
-                        text="Add"
-                        onClick={this.addUsers}
-                    />
-
-                </StandardModal>
+                <AddMemberModal
+                    { ...modalProps(this, "isModalOpen") }
+                />
             </div>
         )
     }
 }
 
-function mapStateToProps(state) {
-    return {
-        users: state.users
-    }
-}
-
-export default connect(mapStateToProps)(InviteMemberButton);
+export default connect()(InviteMemberButton);
