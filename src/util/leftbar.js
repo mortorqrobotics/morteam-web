6e05869fdf9315b71f011ef31f34cf214226861d
AriellaS <ariella.simoni@gmail.com>
2016-10-21 16:47:41 -0700

    Shared redux works for hide leftbar

--- src/util/leftbar.js
+++ src/util/leftbar.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { openLeftbar, closeLeftbar } from "~/shared/actions";
+
+function onToggle(self, str) {
+    self.setState({ [str]: !self.state[str] });
+    if (self.state[str]) {
+        self.props.dispatch(closeLeftbar());
+    } else {
+        self.props.dispatch(openLeftbar());
+    }
+}
 
 export const leftbarProps = (self, str) => {
     return {
         isOpen: self.state[str],
-        onToggle: () => self.setState({ [str]: !self.state[str] }),
+        onToggle: () => onToggle(self, str)
     }
 }
