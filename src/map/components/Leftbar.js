978cc09baa833a28fbdfdf49e93ca5df43a699cb
InsideOutside0 <sammy@salwen.org>
2016-11-04 16:21:16 -0700

    Finished Map page (for the time being)

--- src/map/components/Leftbar.js
+++ src/map/components/Leftbar.js
@@ -2,17 +2,17 @@ import React from "react";
 import Radium from "radium";
 
 import { LeftbarContainer } from "~/shared/components/leftbar";
-import { leftbarProps } from "~/util/leftbar";
 import styles from "~/map/styles";
+import { leftbarProps } from "~/util/leftbar";
 
 import { connect } from "react-redux";
 
 @Radium
 class Leftbar extends React.Component {
 
-    state = {
-        isLeftbarOpen: true,
-    }
+	state = {
+		isLeftbarOpen: true,   
+	}
 
     getTeamDisplay = () => {
         if (this.props.selectedTeam) {
@@ -24,18 +24,21 @@ class Leftbar extends React.Component {
     render() {
 		if(this.props.selectedTeam) {
 			return (
-				<LeftbarContainer>
+				<LeftbarContainer { ...leftbarProps(this, "isLeftbarOpen") }>
 					<li style={styles.teamNum}>
 						{this.getTeamDisplay()}
 					</li>
-					<li>
+					<li style={styles.teamNick}>
 						{this.props.selectedTeam.data.nickname}
 					</li>
-					<li>
+					<li style={styles.teamCountry}>
 						{this.props.selectedTeam.data.region}, {this.props.selectedTeam.data.country_name}
 					</li>
-					<li>   
-						<a href={this.props.selectedTeam.data.website}>{this.props.selectedTeam.data.website}</a>
+					<li style={styles.website}>
+						<a href={this.props.selectedTeam.data.website}>Official Team Website</a>
+					</li>
+					<li style={styles.website}>
+                        <a href={"https://www.thebluealliance.com/team/"+this.props.selectedTeam.num}>Blue Alliance Page</a>
 					</li>
 				</LeftbarContainer>	
 			)
--- src/map/styles.js
+++ src/map/styles.js
@@ -5,5 +5,18 @@ export default {
         fontWeight: "300",
         margin: "15px 0px",
         textAlign: "center",
-    }
+    },
+    teamNick: {
+        textAlign: "center",
+        fontSize: "20px",
+    },
+    teamCountry: {
+        textAlign: "center",
+        fontSize: "20px",
+    },
+    website: {
+        textAlign: "center",
+        fontSize: "20px",
+    },
+
 }
