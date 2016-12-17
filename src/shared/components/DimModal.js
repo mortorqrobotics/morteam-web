651aef226b8fb89cb0d565ab0683d15a493108aa
afuous <benheller42@gmail.com>
2016-12-17 00:43:06 -0800

    Make modal dim lighter, fix 11

--- src/shared/components/DimModal.js
+++ src/shared/components/DimModal.js
@@ -5,7 +5,7 @@ let overlayStyle = {
     position: "fixed",
     top: "0",
     left: "0",
-    backgroundColor: "rgba(0, 0, 0, .6)",
+    backgroundColor: "rgba(0, 0, 0, .3)",
     display: "block",
     zIndex: "100",
 }
