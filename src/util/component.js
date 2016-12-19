be62a8127f0a557c2c677b818a9bccb71d6d8a48
afuous <benheller42@gmail.com>
2016-12-18 20:50:32 -0800

    Make sanitized links open in new tab

--- src/util/component.js
+++ src/util/component.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { sanitize } from "dompurify";
+import DOMPurify, { sanitize } from "dompurify";
 
 export const withCss = (Comp, style) => (
     withProps(Comp, { style: style })
@@ -9,6 +9,18 @@ export const withCss = (Comp, style) => (
 export const withProps = (Comp, values) => (props) => (
     React.cloneElement(React.createElement(Comp, props), values)
 )
+
+// https://github.com/cure53/DOMPurify/blob/master/demos/hooks-target-blank-demo.html#L31
+DOMPurify.addHook("afterSanitizeAttributes", (node) => {
+    if ("target" in node) {
+        node.setAttribute("target", "_blank");
+    }
+    if (!node.hasAttribute("target")
+        && (node.hasAttribute("xlink:href") || node.hasAttribute("href"))
+    ) {
+        node.setAttribute("xlink:show", "new");
+    }
+});
 
 export function allowOnlyTags(tags, str) {
     // const regex = /<(?!(a\s|\/))/;
