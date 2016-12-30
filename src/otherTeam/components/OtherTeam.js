faa74335ef39dc2fc32ed3c11d455f7b1c1bee08
E-A-A <ev34an@gmail.com>
2016-12-30 14:05:20 +0000

    Add basic page structure

--- src/otherTeam/components/OtherTeam.js
+++ src/otherTeam/components/OtherTeam.js
@@ -3,6 +3,9 @@ import Radium from "radium";
 
 import Root, { pageInit } from "~/shared/components/Root";
 import Navbar from "~/shared/components/navbar/Navbar";
+import { pageOptions } from "~/util";
+import { request } from "~/util/ajax";
+import styles from "~/otherTeam/styles"
 
 import { makeStore, soundsMiddleware } from "~/util/redux";
 import sharedReducers from "~/shared/reducers";
@@ -19,11 +22,29 @@ initSIO(socket => initAlertCreator(socket, store.dispatch));
 
 @Radium
 export default class OtherTeam extends React.Component {
-
+    state = {
+        teamInfo: {},
+    };
+    
+    getBlueAllienceInfo = async() => {
+        let { data } = await request("GET", "/teams/number/" + pageOptions.teamNumber + "/info");
+        this.setState({teamInfo: data});
+    }
+    
+    componentDidMount = () => {
+        this.getBlueAllienceInfo();
+    }
+    
     render() {
+        
         return (
             <Root pageName="chat" store={store}>
                 <Navbar />
+                    <h1 style={styles.title}>Team {this.state.teamInfo.team_number} - {this.state.teamInfo.nickname}</h1>
+                    <div style={styles.info}>
+                        <h2>Website: <a href={this.state.teamInfo.website} target="_blank">{this.state.teamInfo.website} </a></h2>
+                        <h2>Loacation: {this.state.teamInfo.location}</h2>
+                    </div>
             </Root>
         )
     }
---
+++ src/otherTeam/styles.js
@@ -0,0 +1,6 @@
+export default {
+    title: {
+        textAlign: "center",
+        marginTop: "50px",
+    }
+}
