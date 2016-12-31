4a04a4cdd567c782921f343f760541cff80e9972
AriellaS <ariella.simoni@gmail.com>
2016-12-30 20:45:31 -0800

    Display confirm modal when attempting to leave group

--- src/group/components/LeaveGroupButton.js
+++ src/group/components/LeaveGroupButton.js
@@ -3,24 +3,33 @@ import Radium from "radium";
 
 import styles from "~/group/styles";
 import Button from "~/shared/components/forms/Button";
+import ConfirmModal from "~/shared/components/ConfirmModal";
 import { currentUser, pageOptions } from "~/util";
+import { modalProps } from "~/util/modal";
 import { connect } from "react-redux";
 import { leaveGroup } from "~/group/actions";
 
 @Radium
 class LeaveGroupButton extends React.Component {
 
-    handleClick = () => {
-        this.props.dispatch(leaveGroup());
+    state = {
+        isModalOpen: false,
     }
 
     render() {
         return (
-            <Button
-                style={styles.buttons.leaveButton}
-                value="Leave"
-                onClick={this.handleClick}
-            />
+            <div>
+                <Button
+                    style={styles.buttons.leaveButton}
+                    value="Leave"
+                    onClick={() => this.setState({ isModalOpen: true })}
+                />
+                <ConfirmModal
+                    text="Are you sure you would like to leave the group?"
+                    action={() => this.props.dispatch(leaveGroup())}
+                    {...modalProps(this, "isModalOpen")}
+                />
+            </div>
         )
     }
 }
