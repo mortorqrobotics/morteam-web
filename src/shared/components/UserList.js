ae9ebc3102e301c626e834a74b3e9cf832bb9bf0
Ariella Simoni <ariella.simoni@gmail.com>
2017-01-01 18:05:14 -0800

    Long names in userlist (#74)
    * Adjust font size of names based on size of userlabel boxes
    
    * Fix userlabel onClick
    
    * Replace ref name with something simpler
    
    * Add a condition in the while loop to prevent infinite loops

--- src/shared/components/UserList.js
+++ src/shared/components/UserList.js
@@ -8,19 +8,50 @@ import ProfilePicture from "~/shared/components/ProfilePicture";
 import ConfirmModal from "~/shared/components/ConfirmModal";
 import { fullName, currentUser } from "~/util";
 import { modalProps } from "~/util/modal";
-import styles from "~/shared/styles/userList";
+import styles, { nameFontSize } from "~/shared/styles/userList";
 
 const RadiumGlyphicon = Radium(Glyphicon);
+const userLabelHeight = 70;
 
 @Radium
 class UserLabel extends React.Component {
+
     state = { isModalOpen: false };
+
+    adjustFontSize = () => {
+        // removes "px" at end
+        let fontSize = parseInt(this.refs.name.style.fontSize.slice(0, -2));
+
+        // makes font as large as possible under the default size w/ out increasing box height
+        while (this.refs.name.parentElement.clientHeight <= userLabelHeight
+            && fontSize < nameFontSize
+        ) {
+            fontSize++;
+            this.refs.name.style.setProperty("font-size", fontSize + "px");
+        }
+
+        //makes font small enough so the box is not expanded
+        while (this.refs.name.parentElement.clientHeight > userLabelHeight
+            && fontSize > 0
+        ) {
+            fontSize--;
+            this.refs.name.style.setProperty("font-size", fontSize + "px");
+        }
+    }
+
+    componentDidMount = () => {
+        this.adjustFontSize();
+        window.addEventListener("resize", this.adjustFontSize);
+    }
+
     render() {
         return (
             <Col sm={6} md={4} lg={3}>
                 <span
                     style={styles.userDisplay.span}
-                    onClick={() => window.location.assign("/profiles/id/" + user._id)}
+                    onClick={() =>
+                        window.location.assign("/profiles/id/" + this.props.user._id)
+                    }
                 >
                     <ProfilePicture
                         user={this.props.user}
@@ -28,7 +59,7 @@ class UserLabel extends React.Component {
                         frameSize={30}
                         hasIndicator
                     />
-                    <span style={styles.userDisplay.name}>
+                    <span style={styles.userDisplay.name} ref="name">
                         {fullName(this.props.user)}
                     </span>
                     {currentUser.isAdmin() && (
