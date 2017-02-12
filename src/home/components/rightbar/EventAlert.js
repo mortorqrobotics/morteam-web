02c381292266174341d3e28b7b9bc16b1ec70651
InsideOutside0 <sammy@salwen.org>
2017-02-12 16:43:52 -0500

    Make event alerts display when they should - styling still needs work

--- src/home/components/rightbar/EventAlert.js
+++ src/home/components/rightbar/EventAlert.js
@@ -3,23 +3,60 @@ import Radium from "radium";
 
 import styles from "~/home/styles/leftbar";
 import LeftbarButton from "../leftbar/LeftbarButton";
+import ajax from "~/util/ajax";
+import { currentUser } from "~/util";
 
 @Radium
 export default class EventAlert extends React.Component {
 
+    state = {
+        events: [],
+    }
+
+    componentDidMount = async () => {
+        await this.getRecentEvents();
+    }
+
+    getRecentEvents = async () => {
+        let todayEvents = [];
+        let today = new Date();
+        let { data } = await ajax.request("GET", "/events"
+            + `/startYear/${today.getFullYear()}/startMonth/${today.getMonth()}`
+            + `/endYear/${today.getFullYear()}/endMonth/${today.getMonth()}`
+        );
+        data = data.filter(event => new Date(event.date).getDate() == today.getDate());
+        todayEvents = data.filter(event => event.hasTakenAttendance == false);
+        console.log(todayEvents.length);
+        this.setState({
+            events: todayEvents,
+        });
+    }
+
+    renderAttendanceAlert = () => {
+        if (currentUser.isAdmin() && this.state.events.length > 0) {
+            const attendanceAlerts = this.state.events.map(event => (
+                <div key={event._id}>
+                    <h3>Event Today</h3>
+                    <p>The event, <span>{event.name}</span>
+                        , has begun!</p>
+                    <LeftbarButton
+                        text="Click to take Attendance"
+                    />
+                </div>
+            ))
+            return (
+            <div style= {styles.leftbar.alert}>
+                {attendanceAlerts}
+            </div>
+            )
+        }
+    }
+
     render () {
         return (
-            <div style= {styles.leftbar.alert}>
-                <h3>Alert!</h3>
-                <p style={styles.leftbar.p}>The event,
-                    <span>TestEvent</span> has begun!
-                </p>
-                <LeftbarButton
-                    text="Take Attendance"
-                />
-
+            <div>
+                {this.renderAttendanceAlert()}
             </div>
-
         )
     }
 
--- src/home/components/rightbar/Rightbar.js
+++ src/home/components/rightbar/Rightbar.js
@@ -2,26 +2,17 @@ import React from "react";
 import Radium from "radium";
 
 import EventAlert from "./EventAlert";
+
 import { currentUser } from "~/util";
 import styles from "~/home/styles/leftbar";
 
 @Radium
 export default class Rightbar extends React.Component {
 
-    renderAttendanceAlert = () => {
-        if (currentUser.isAdmin()) {
-            return (
-                <EventAlert />
-            )
-
-        }
-
-    }
-
     render() {
         return (
             <div style={styles.leftbar.div}>
-                {this.renderAttendanceAlert()}
+                <EventAlert />
             </div>
 
         )
