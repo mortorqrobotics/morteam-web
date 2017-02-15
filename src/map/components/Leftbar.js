e7f0b7f0ea81749d7c93a09d8527e60c2c8127c2
AriellaS <ariella.simoni@gmail.com>
2017-02-14 16:25:18 -0800

    Fix map leftbar

--- src/map/components/Leftbar.js
+++ src/map/components/Leftbar.js
@@ -18,39 +18,35 @@ class Leftbar extends React.Component {
     }
 
     render() {
-        if (this.props.selectedTeam) {
-            return (
-                <LeftbarContainer { ...leftbarProps(this) }>
-                    <li style={styles.teamNum}>
-                        {this.getTeamDisplay()}
-                    </li>
-                    <li style={styles.teamNick}>
-                        {this.props.selectedTeam.data.nickname}
-                    </li>
-                    <li style={styles.teamCountry}>
-                        {this.props.selectedTeam.data.region}, {this.props.selectedTeam.data.country_name}
-                    </li>
-                    <li style={styles.website}>
-                        <a href={this.props.selectedTeam.data.website}>Official Team Website</a>
-                    </li>
-                    <li style={styles.website}>
-                        <a href={"https://www.thebluealliance.com/team/" + this.props.selectedTeam.num}>
-                            Blue Alliance Page
-                        </a>
-                    </li>
-                    <li style={styles.website}>
-                        <a href={"/teams/number/" + this.props.selectedTeam.num}>
-                            MorTeam page
-                        </a>
-                    </li>
-                </LeftbarContainer>
-            )
-        }
         return (
             <LeftbarContainer { ...leftbarProps(this) }>
                 <li style={styles.teamNum}>
                     {this.getTeamDisplay()}
                 </li>
+
+                {this.props.selectedTeam && (
+                    <div>
+                        <li style={styles.teamNick}>
+                            {this.props.selectedTeam.data.nickname}
+                        </li>
+                        <li style={styles.teamCountry}>
+                            {this.props.selectedTeam.data.region}, {this.props.selectedTeam.data.country_name}
+                        </li>
+                        <li style={styles.website}>
+                            <a href={this.props.selectedTeam.data.website}>Official Team Website</a>
+                        </li>
+                        <li style={styles.website}>
+                            <a href={"https://www.thebluealliance.com/team/" + this.props.selectedTeam.num}>
+                                Blue Alliance Page
+                            </a>
+                        </li>
+                        <li style={styles.website}>
+                            <a href={"/teams/number/" + this.props.selectedTeam.num}>
+                                MorTeam page
+                            </a>
+                        </li>
+                    </div>
+                )}
             </LeftbarContainer>
         )
     }
