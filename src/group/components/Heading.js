26a51978fceb9bae219d6fa6a94d4921176577f6
AriellaS <ariella.simoni@gmail.com>
2019-03-30 22:38:52 -0700

    Add delete group button

--- src/group/components/Heading.js
+++ src/group/components/Heading.js
@@ -5,12 +5,21 @@ import styles from "~/group/styles";
 import LeaveGroupButton from "~/group/components/LeaveGroupButton";
 import InviteMemberButton from "~/group/components/InviteMemberButton";
 import JoinButton from "~/group/components/JoinButton";
+import ConfirmModal from "~/shared/components/ConfirmModal";
+import { modalProps } from "~/util/modal";
+import Button from "~/shared/components/forms/Button";
 import { getGroupName } from "~/util/groups";
-import { connect } from "react-redux";
 import { currentUser } from "~/util";
+
+import { deleteGroup } from "~/group/actions";
+import { connect } from "react-redux";
 
 @Radium
 class Heading extends React.Component {
+
+    state = {
+        isModalOpen: false,
+    }
 
     renderConditionalButtons = () => {
         if (this.props.group.__t == "NormalGroup") {
@@ -18,10 +27,23 @@ class Heading extends React.Component {
                 return (
                     <div>
                         {currentUser.isAdmin() && (
-                            <InviteMemberButton/>
+                            <div>
+                                <InviteMemberButton/>
+                                <LeaveGroupButton />
+                                <Button
+                                    value="Delete"
+                                    style={{ marginBottom: "50px" }}
+                                    onClick={() => this.setState({ isModalOpen: true })}
+                                />
+                                        <ConfirmModal
+                                            grayConfirm
+                                            text={`Are you sure you would like to delete ${getGroupName(this.props.group)}?`}
+                                            action={() => deleteGroup()}
+                                            { ...modalProps(this, "isModalOpen") }
+                                        />
+                                            </div>
                         )}
-                        <LeaveGroupButton />
-                    </div>
+                            </div>
                 )
             } else {
                 return (
@@ -32,7 +54,6 @@ class Heading extends React.Component {
     }
 
     render() {
-        console.log(this.props.group);
         return (
             <div>
                 <h1 style={styles.groupName}>
