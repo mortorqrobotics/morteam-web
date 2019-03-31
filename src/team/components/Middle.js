8735e558f422ce175f3b4d0cee7cf80c36dc8e1d
Ariella Simoni <ariella.simoni@gmail.com>
2019-03-31 00:37:35 -0700

    Put all members of position alumnus below current members in the all team page (#115)

--- src/team/components/Middle.js
+++ src/team/components/Middle.js
@@ -2,31 +2,47 @@ import React from "react";
 import Radium from "radium";
 
 import UserList from "~/shared/components/UserList";
+import styles from "~/team/styles";
 import { deleteUser } from "~/team/actions";
 import { connect } from "react-redux";
+
+const text = "Warning: Removing someone from a team is not"
+    + " easily reversible. Do not do this unless you"
+    + " really mean to.";
 
 @Radium
 class Middle extends React.Component {
 
     render() {
         return (
-            <UserList
-                users={this.props.users}
-                deleteModal={{
-                    handleDeleteUser: (userId) => this.props.dispatch(deleteUser(userId)),
-                    text: "Warning: Removing someone from a team is not"
-                        + " easily reversible. Do not do this unless you"
-                        + " really mean to.",
-                    grayConfirm: true,
-                }}
-            />
+            <div>
+                <UserList
+                    users={this.props.currentMembers}
+                    deleteModal={{
+                        handleDeleteUser: (userId) => this.props.dispatch(deleteUser(userId)),
+                        text,
+                        grayConfirm: true,
+                    }}
+                />
+                <h2 style={styles.h2}>Alumni</h2>
+                <hr style={styles.hr} />
+                <UserList
+                    users={this.props.alumni}
+                    deleteModal={{
+                        handleDeleteUser: (userId) => this.props.dispatch(deleteUser(userId)),
+                        text,
+                        grayConfirm: false,
+                    }}
+                />
+            </div>
         )
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        users: state.users,
+        currentMembers: state.users.filter(u => u.position !== "alumnus"),
+        alumni: state.users.filter(u => u.position === "alumnus"),
     }
 }
 
